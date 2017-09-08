b288ebbed536fa0792da26017a69e25f7a71e09a
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-08 14:48:30 +0200

    add htm select variables

---
+++ app.js
@@ -0,0 +1,10 @@
+// html select
+var zScoreElement = document.getElementById('z-score-input-value');
+var tScoreElement = document.getElementById('t-score-input-value');
+var iqScoreElement = document.getElementById('iq-score-input-value');
+var wechScoreElement = document.getElementById('wech-score-input-value');
+var clickButtonElement = document.getElementById('click-button');
+
+clickButtonElement.addEventListener('click', function () {
+    console.log('kliknul sem na button');
+});
