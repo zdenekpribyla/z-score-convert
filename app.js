e10367081aa7361767f1f22fb0f76f220bb9b0b1
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-08 18:03:19 +0200

    add first pattern for t score

--- app.js
+++ app.js
@@ -5,9 +5,25 @@ var iqScoreElement = document.getElementById('iq-score-input-value');
 var wechScoreElement = document.getElementById('wech-score-input-value');
 var clickButtonElement = document.getElementById('click-button');
 
+var getValues = function () {
+    console.log('getValues');
+    return {
+        zScore: Number(zScoreElement.value),
+        tScore: Number(tScoreElement.value)
+    }
+};
+
+
+
+
+var calculate = function (inputData) {
+    return (10 * inputData.zScore) - 50;
+
+};
+
 clickButtonElement.addEventListener('click', function () {
     console.log('kliknul sem na button');
-    tScoreElement.value = 4
+    tScoreElement.value = calculate(getValues())
 });
 
 
