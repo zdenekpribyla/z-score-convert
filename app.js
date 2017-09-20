bc608effae43c334c6ded063b94c2d9cf02f30f5
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-09-20 11:57:00 +0200

    add code for percentile calculation

--- app.js
+++ app.js
@@ -187,7 +187,67 @@ var calculateStanine = function () {
 };
 
 
-// var stenResult = calculateSten();
+// percentile convertor
+var Z_MAX = 6;
+
+function pzscore(z)
+{
+    var y, x, w;
+    if (z == 0.0)
+    {
+        x = 0.0;
+    }
+    else
+    {
+        y = 0.5 * Math.abs(z);
+        if (y > (Z_MAX * 0.5))
+        {
+            x = 1.0;
+        }
+        else if (y < 1.0)
+        {
+            w = y * y;
+            x = ((((((((0.000124818987 * w
+                - 0.001075204047) * w + 0.005198775019) * w
+                - 0.019198292004) * w + 0.059054035642) * w
+                - 0.151968751364) * w + 0.319152932694) * w
+                - 0.531923007300) * w + 0.797884560593) * y * 2.0;
+        }
+        else
+        {
+            y -= 2.0;
+            x = (((((((((((((-0.000045255659 * y
+                + 0.000152529290) * y - 0.000019538132) * y
+                - 0.000676904986) * y + 0.001390604284) * y
+                - 0.000794620820) * y - 0.002034254874) * y
+                + 0.006549791214) * y - 0.010557625006) * y
+                + 0.011630447319) * y - 0.009279453341) * y
+                + 0.005353579108) * y - 0.002141268741) * y
+                + 0.000535310849) * y + 0.999936657524;
+        }
+    }
+    return z > 0.0 ? ((x + 1.0) * 0.5) : ((1.0 - x) * 0.5);
+}
+
+function calc()
+{
+    var z = document.getElementById("z").value;
+    if (Math.abs(z) > Z_MAX)
+    {
+        alert("Enter z value must be between -6 and 6.");
+    }
+    else
+    {
+        var lp = pzscore(z);
+        document.getElementById("ltp").value = (lp * 100).toFixed(2)+" %";
+        var rp = 1 - lp;
+        document.getElementById("rtp").value = (rp * 100).toFixed(2)+" %";
+        var tp = 2 * rp;
+        document.getElementById("ttp").value = (tp * 100).toFixed(2)+" %";
+        var cl = 1 - tp;
+        document.getElementById("clp").value = (cl * 100).toFixed(2)+" %";
+    }
+}
 
 
 
